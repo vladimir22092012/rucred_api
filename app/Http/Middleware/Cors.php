0043cfe8f067d951c45e52d20fb7d1e4eb94b2aa
Андрей Попов <duircianos@yandex.ru>
2023-01-05 20:02:21 +0300

    first

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -14,8 +14,10 @@ class Cors
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
