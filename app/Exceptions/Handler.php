7985a775c806e6379368272b7e3b19fc1086083d
Андрей Попов <duircianos@yandex.ru>
2023-01-05 18:56:35 +0300

    first

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -58,7 +58,7 @@ class Handler extends ExceptionHandler
     {
         if ($this->isHttpException($exception))
             if ($exception->getStatusCode() == 404)
-                return response()->view('errors.' . '404', [], 404);
+                return 'method not allowed';
 
 
         return parent::render($request, $exception);
