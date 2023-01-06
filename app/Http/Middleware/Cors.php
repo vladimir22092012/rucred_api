15c9a7f46c9897a68a80b8b0f858d738157f3711
Андрей Попов <duircianos@yandex.ru>
2023-01-06 12:58:29 +0300

    first

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,6 +20,6 @@ class Cors
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
-            ->header('Access-Control-Allow-Credentials',' true');
+            ->header('Access-Control-Allow-Credentials',' false');
     }
 }
