c5cb9bc101534d2f158366fa2634b2ad00dda961
Андрей Попов <duircianos@yandex.ru>
2023-01-10 15:27:13 +0300

    фото юзера

---
+++ app/Http/Middleware/OrderOwner.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Orders;
+use App\Models\UsersTokens;
+use Closure;
+use Illuminate\Http\Request;
+
+class OrderOwner
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $orderId = $request['orderId'];
+
+        $token = $request->header('Authorization');
+        $userToken = UsersTokens::where('token', $token)->orderBy('id', 'desc')->first();
+
+        $order = Orders::find($orderId);
+
+        if($order->user_id != $userToken->user_id)
+            return $next($request);
+        else
+            return response()->json(['status' => 404, 'resp' => 'it is not your order']);
+    }
+}
