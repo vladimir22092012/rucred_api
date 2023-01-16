17562fc2b8df5838217ba4813f873cc3c5de4889
Андрей Попов <duircianos@yandex.ru>
2023-01-16 15:01:47 +0300

    фото юзера

--- app/Models/Companies.php
+++ app/Models/Companies.php
@@ -9,4 +9,14 @@ class Companies extends Model
     protected $table = 's_companies';
     protected $guarded = [];
     public $timestamps = false;
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function branches()
+    {
+        return $this->hasMany(Branch::class);
+    }
 }
