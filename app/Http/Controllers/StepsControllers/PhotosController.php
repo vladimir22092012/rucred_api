70f8872d40a9a91222ced9015dc5aad0d9dfa02a
Андрей Попов <duircianos@yandex.ru>
2023-01-16 17:07:53 +0300

    фото юзера

---
+++ app/Http/Controllers/StepsControllers/PhotosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\StepsControllers;
+
+use Illuminate\Http\Request;
+
+class PhotosController extends StepsController
+{
+    public function action(Request $request)
+    {
+        $userId = self::$userId;
+        $type = $params['type'];
+    }
+}
