f8527a3efb467a6382e401d94601bab320b03587
rukred <rukred@rukred.com>
2023-02-08 14:23:02 +0300

    commit

--- app/Tools/PaymentSchedule.php
+++ app/Tools/PaymentSchedule.php
@@ -16,18 +16,11 @@ class PaymentSchedule extends Tools
     {
         $amount = $data['amount'];
         $start_date = $data['start_date'];
-        $end_date = $data['end_date'];
         $first_pay_day = $data['first_pay_day'];
         $percent = $data['percent'];      //параметр тарифа с учетом профсоюза
         $free_period = $data['free_period'];  //параметр тарифа
         $min_period = $data['min_period'];   //параметр тарифа
         $orderId = $data['order_id'] ?? null;
-
-        $percent_per_month = (($percent / 100) * 360) / 12;
-
-        $period = 1;
-
-        $probably_return_sum = $amount * ($percent_per_month / (1 - pow((1 + $percent_per_month), -$period)));
 
         //адаптированный код из црм
         $rest_sum = $amount;
@@ -38,35 +31,19 @@ class PaymentSchedule extends Tools
         if ($start_date > $paydate || date_diff($paydate, $start_date)->days <= $free_period)
             $paydate->add(new \DateInterval('P1M'));
 
-        $annoouitet_pay = $probably_return_sum;
-        $annoouitet_pay = round($annoouitet_pay, 2);
-
-        $iteration = 0;
-
-        $count_days_this_month = date('t', strtotime($start_date->format('Y-m-d')));
-
         $paydate = Utils::processing('check_pay_date', new \DateTime($paydate->format('Y-m-' . $first_pay_day)));
 
-        if (date_diff($paydate, $start_date)->days <= $free_period) {
-            $plus_loan_percents = round(($percent / 100) * $amount * date_diff($paydate, $start_date)->days, 2);
-            $sum_pay = $annoouitet_pay + $plus_loan_percents;
-            $loan_percents_pay = round(($rest_sum * $percent_per_month) + $plus_loan_percents, 2);
-            $body_pay = $sum_pay - $loan_percents_pay;
-            $paydate->add(new \DateInterval('P1M'));
-            $iteration++;
-        } elseif (date_diff($paydate, $start_date)->days >= $min_period && date_diff($paydate, $start_date)->days < $count_days_this_month) {
-            $body_pay = $rest_sum;
-            $loan_percents_pay = $amount * ($percent/100) * date_diff($paydate, $start_date)->days;
-            $sum_pay = $body_pay + $loan_percents_pay;
-            $iteration++;
-        } elseif (date_diff($paydate, $start_date)->days >= $count_days_this_month) {
-            $body_pay = $rest_sum;
-            $loan_percents_pay = $amount * ($percent/100) * date_diff($paydate, $start_date)->days;
-            $sum_pay = $body_pay + $loan_percents_pay;
+        if(date_diff($paydate, $start_date)->days <= $free_period)
+            $paydate->add(new DateInterval('P1M'));
+
+        if (date_diff($paydate, $start_date)->days > $free_period && date_diff($paydate, $start_date)->days < $min_period) {
+            $loan_percents_pay = round(($percent / 100) * $amount * date_diff($paydate, $start_date)->days, 2);
+            $body_pay = 0;
+            $sum_pay = $loan_percents_pay;
         } else {
-            $sum_pay = ($percent / 100) * $amount * date_diff($paydate, $start_date)->days;
-            $loan_percents_pay = $sum_pay;
-            $body_pay = 0.00;
+            $loan_percents_pay = round($amount * ($percent / 100) * date_diff($paydate, $start_date)->days);
+            $body_pay = $amount;
+            $sum_pay = $loan_percents_pay + $body_pay;
         }
 
         $payment_schedule[$paydate->format('d.m.Y')] =
@@ -77,41 +54,43 @@ class PaymentSchedule extends Tools
                 'comission_pay' => 0.00,
                 'rest_pay' => $rest_sum -= $body_pay
             ];
-        $paydate->add(new \DateInterval('P1M'));
 
-        $period -= $iteration;
+        if (!empty($orderId))
+            Orders::where('id', $orderId)->update(['probably_return_date' => date('Y-m-d H:i:s', strtotime($paydate->format('d.m.Y')))]);
+
+        $paydate->add(new \DateInterval('P1M'));
 
         if ($rest_sum != 0) {
 
-            for ($i = 1; $i <= $period; $i++) {
-                $paydate->setDate($paydate->format('Y'), $paydate->format('m'), $first_pay_day);
+
+            $paydate->setDate($paydate->format('Y'), $paydate->format('m'), $first_pay_day);
+            $date = Utils::processing('check_pay_date', $paydate);
+
+            if (isset($payment_schedule[$date->format('d.m.Y')])) {
+                $date = self::add_month($date->format('d.m.Y'), 2);
+                $paydate->setDate($date->format('Y'), $date->format('m'), $first_pay_day);
                 $date = Utils::processing('check_pay_date', $paydate);
-
-                if (isset($payment_schedule[$date->format('d.m.Y')])) {
-                    $date = self::add_month($date->format('d.m.Y'), 2);
-                    $paydate->setDate($date->format('Y'), $date->format('m'), $first_pay_day);
-                    $date = Utils::processing('check_pay_date', $paydate);
-                }
-
-                $loan_body_pay = $rest_sum;
-                $loan_percents_pay = $amount * ($percent / 100) * date_diff($start_date, $date)->days - $loan_percents_pay;
-                $annoouitet_pay = $loan_body_pay + $loan_percents_pay;
-                $rest_sum = 0.00;
-
-                $payment_schedule[$date->format('d.m.Y')] =
-                    [
-                        'pay_sum' => $annoouitet_pay,
-                        'loan_percents_pay' => $loan_percents_pay,
-                        'loan_body_pay' => $loan_body_pay,
-                        'comission_pay' => 0.00,
-                        'rest_pay' => $rest_sum
-                    ];
-
-                if(!empty($orderId))
-                    Orders::where('id', $orderId)->update(['probably_return_date' => date('Y-m-d H:i:s', strtotime($date->format('d.m.Y')))]);
-
-                $paydate->add(new \DateInterval('P1M'));
             }
+
+            $loan_body_pay = $rest_sum;
+            $loan_percents_pay = $amount * ($percent / 100) * date_diff($start_date, $date)->days - $loan_percents_pay;
+            $annoouitet_pay = $loan_body_pay + $loan_percents_pay;
+            $rest_sum = 0.00;
+
+            $payment_schedule[$date->format('d.m.Y')] =
+                [
+                    'pay_sum' => $annoouitet_pay,
+                    'loan_percents_pay' => $loan_percents_pay,
+                    'loan_body_pay' => $loan_body_pay,
+                    'comission_pay' => 0.00,
+                    'rest_pay' => $rest_sum
+                ];
+
+            if (!empty($orderId))
+                Orders::where('id', $orderId)->update(['probably_return_date' => date('Y-m-d H:i:s', strtotime($date->format('d.m.Y')))]);
+
+            $paydate->add(new \DateInterval('P1M'));
+
         }
 
         $payment_schedule['result'] =
@@ -203,6 +182,9 @@ class PaymentSchedule extends Tools
                 'rest_pay' => $rest_sum -= $body_pay
             ];
 
+        if (!empty($orderId))
+            Orders::where('id', $orderId)->update(['probably_return_date' => date('Y-m-d H:i:s', strtotime($paydate->format('d.m.Y')))]);
+
         $paydate->add(new \DateInterval('P1M'));
 
         $period -= $iteration;
@@ -238,7 +220,7 @@ class PaymentSchedule extends Tools
                         'rest_pay' => $rest_sum
                     ];
 
-                if(!empty($orderId))
+                if (!empty($orderId))
                     Orders::where('id', $orderId)->update(['probably_return_date' => date('Y-m-d H:i:s', strtotime($date->format('d.m.Y')))]);
 
                 $paydate->add(new \DateInterval('P1M'));
